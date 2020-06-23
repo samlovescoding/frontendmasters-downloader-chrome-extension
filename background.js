58c48c586eb7d1b3e2277e66203af2189fc1c9d7
Sampan Verma <sampan.developer@gmail.com>
2020-06-23 18:18:56 +0530

    Made filenames URL Safe

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.action == "videoFound"){
         src = request.src
-        video = request.video
-        course = request.course
+        video = request.video.replace(/[^a-zA-Z0-9]/gi, '_')
+        course = request.course.replace(/[^a-zA-Z0-9]/gi, '_')
         index = request.index
         extension = src.split(".").pop().split(/\#|\?/g)[0]
         filename = "Frontend Masters/" + course + "/" + index + ". " + video + "." + extension
